e5c9702da53f6e3d0b7bb6dac5c4fa763ee5a5d6
mihrimahsenakucuk <73707051+mihrimahsenakucuk@users.noreply.github.com>
2021-02-07 21:00:52 +0300

    Update Q01.java

--- src/Q01.java
+++ src/Q01.java
@@ -2,14 +2,14 @@ public class Q01 {
     public static void main(String[] args) {
 
         Scanner scan=new Scanner(System.in);
+        System.out.println("Kelimenizi giriniz");
+        String kelime= scan.next();
 
-        String word= scan.next();
-
-        if (word.length()%2==1 && word.length()>=3) {
-            System.out.println("The word has odd number od characters");
-            System.out.println(word.substring(word.length()/2,word.length()/2+1));
+        if (kelime.length()%2==1 && kelime.length()>=3) {
+            System.out.println("Kelimenizdeki karakter sayısı tek.");
+            System.out.println(kelime.substring(kelime.length()/2,kelime.length()/2+1));
         }else {
-            System.out.println("Is has not odd number character");
+            System.out.println("Kelimenizdeki karakter sayısı çift.");
         }
 
     }
