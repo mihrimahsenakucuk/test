7396cfc26d60f890a1a621143f2a236a339d88b5
sena <mskucukk@outlook.com>
2021-02-07 23:36:30 +0300

    Q2

--- src/Q02.java
+++ src/Q02.java
@@ -1,35 +1,44 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Q02 {
-    public static void main(String[] args){
-        /*
-        1)Kullanıcıdan array'in uzunluğunu girmesini isteyin.
-        2)sort method kullanarak array'i konsola artan ve azalan sırada yazdırın.
-         */
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Array'in uzunluğunu giriniz");
-        int uzunluk=scan.nextInt();
-        int arr[]= new int[uzunluk];
 
-        System.out.println("Array'in elemanlarını giriniz");
-        for(int i=0 ; i<uzunluk ; i++){
+    public static void main(String[] args) {
+		/*
+		1)Kullanıcıdan tamsayı dizisi girmesini isteyin
+		2)sort method kullanarak diziyi artan azalan olarak yazdırın.
+		 */
+
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Dizinin uzunluğunu giriniz");
+        int uzunluk=scan.nextInt();
+        int arr[]=new int[uzunluk];
+
+        System.out.println("Dizinin elemanlarını giriniz");
+        for(int i=0;i<uzunluk;i++) {
             arr[i]=scan.nextInt();
         }
-        for(int i=0 ; i<uzunluk ; i++){
-            System.out.print(arr[i] + " ");
+        for(int i=0;i<uzunluk;i++) {
+            System.out.println(arr[i]+" ");
         }
+
         System.out.println();
         Arrays.sort(arr);
 
-        //artan sıralama
-        for(int i=0 ; i<uzunluk ; i++){
-            System.out.println(arr[i] + " ");
+        //artan sıra
+        for(int i=0; i<uzunluk;i++) {
+            System.out.print(arr[i] + "  ");
         }
 
-        //azalan sıralama
-        for(int i= 0; i<uzunluk ; i++){
-            System.out.println(arr[uzunluk-1-i] + " ");
+        //azalan sıra
+        for(int i=0; i<uzunluk;i++) {
+            System.out.print(arr[uzunluk-1-i] + "  ");
         }
+
+
     }
+
 }
+
+
+
